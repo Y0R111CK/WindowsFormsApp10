bf7b14ff52ecef4325e3bb4d7b962d196a155f19
Y0R111CK <103890630+Y0R111CK@users.noreply.github.com>
2022-05-21 12:11:39 +0300

    Add files via upload

---
+++ WindowsFormsApp10/WindowsFormsApp10/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();         
+            g.DrawEllipse(Pens.Black, 110, 5, 75, 75);//Голова
+            g.FillEllipse(Brushes.Black, 110, 5, 75, 75);
+
+            SolidBrush a = new SolidBrush(Color.Black);// Л рука
+            g.FillRectangle(a, 50, 80, 50, 120);
+
+            SolidBrush b = new SolidBrush(Color.Black);// Тело
+            g.FillRectangle(b, 110, 80, 80, 200);
+
+            SolidBrush c = new SolidBrush(Color.Black);// П рука
+            g.FillRectangle(c, 200, 80, 50, 120);
+
+            SolidBrush d = new SolidBrush(Color.Black);// Л нога
+            g.FillRectangle(d, 110, 290, 35, 200);
+
+            SolidBrush r = new SolidBrush(Color.Black);// П нога
+            g.FillRectangle(r, 155, 290, 35, 200); 
+        }
+    }
+}
